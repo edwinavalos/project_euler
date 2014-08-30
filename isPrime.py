675a87c576005c63bff2ec7a4252475761e02476
Edwin Avalos <edwin.avalos@code42.com>
2014-08-30 15:02:49 -0500

    Finished problem 10

--- isPrime.py
+++ isPrime.py
@@ -1,5 +1,6 @@
 def isPrime(x):
-    if x==2: return True 
+    if x==1: return False
+    elif x==2: return True 
     for i in range(2,int(x**.5)+1):
         if x % i == 0: return False
     else: return True
---
+++ problem_10.py
@@ -0,0 +1,3 @@
+from isPrime import isPrime
+
+print sum([x for x in xrange(2,2000000) if isPrime(x)])
